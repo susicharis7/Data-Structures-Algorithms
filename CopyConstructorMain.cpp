99985d0cdd2ad208af2e422f63d1dcec994ab34d
susicharis7 <susicharis99@gmail.com>
2025-03-24 00:50:24 +0100

    main for 2nd and 3rd rule

--- CopyConstructorMain.cpp
+++ CopyConstructorMain.cpp
@@ -1,5 +1,24 @@
-//
-// Created by Haris on 23. 3. 2025..
-//
+#include "Week_1_Revision/CopyConstructor&Assignment(2&3Rule)/Student.h"
 
-#include "CopyConstructorMain.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // // COPY CONSTRUCTOR
+    // Student s1("Haris",20,9.4);
+    // Student s2 = s1;
+    // Student s3(s1);
+
+    // cout << "\nEND OF COPY CONSTRUCTOR! " << endl;
+
+    // COPY ASSIGNMENT
+    Student s4("Sumea",20,9.9);
+    Student s5("Semha",17,9.5);
+    s5 = s4;
+
+
+}
+
+
