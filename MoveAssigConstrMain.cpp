a8eb307af105c83805780d9f5005190d88db08ac
susicharis7 <susicharis99@gmail.com>
2025-03-24 00:50:03 +0100

    Main for 4th and 5th Rule

--- MoveAssigConstrMain.cpp
+++ MoveAssigConstrMain.cpp
@@ -1,5 +1,9 @@
-//
-// Created by Haris on 23. 3. 2025..
-//
+#include <iostream>
+#include "Week_1_Revision/MoveConstr_Assignment(4_5Rule)/SecStudent.h"
+using namespace std;
+int main() {
+    SecStudent s1("Sumea",20,9.5);
+    SecStudent s2 = move(s1);
+    s2.display();
+}
 
-#include "MoveAssigConstrMain.h"
